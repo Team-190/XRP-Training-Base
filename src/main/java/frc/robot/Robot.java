4bf15cf763f4b02e326e4ea83f00247e88b80441
ElliotScher <ecscher84@gmail.com>
2026-09-06 18:39:42 -0400

    remove modifiers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
 
 public class Robot extends TimedRobot {
-    private final XRPMotor motor = new XRPMotor(0);
+    XRPMotor motor = new XRPMotor(0);
 
     @Override
     public void robotPeriodic() {
